5cbe720df7ba7d6f422af678e7984df837e8525a
cpk <ckahle33@gmail.com>
2015-08-07 02:23:49 -0500

    logout stuff

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,10 +10,10 @@ module SessionsHelper
   end
 
 	def current_user
-    if session[:user_id]
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
-    elsif cookies.signed[:user_id]
-      user = User.find_by(id: cookies.signed[:user_id])
+    elsif (user_id  = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
@@ -28,5 +28,18 @@ module SessionsHelper
   def log_out 
   	session.delete(:user_id)
   	@current_user = nil
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+    @current_user = nil
+  end
+
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
